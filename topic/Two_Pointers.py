bcf60e05625e0e5eb61e007017e2579f714791a8
Jiin Kim <>
2026-04-27 16:57:58 -0700

    4/27 update

--- topic/Two_Pointers.py
+++ topic/Two_Pointers.py
@@ -23,15 +23,25 @@
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.isAlphanum(s[l]):
+                l += 1
+            while l < r and not self.isAlphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
 
-    def isAlphanum(self, c)
-        if (ord('A') <= ord(c) <= ord('Z') or 
-            ord('a') <= ord(c) <= ord('z') or 
+    def isAlphanum(self, c):
+        if (ord('A') <= ord(c) <= ord('Z') or
+            ord('a') <= ord(c) <= ord('z') or
             ord('0') <= ord(c) <= ord('9')):
             return True
         return False
         
-
 
 #########################################################################################
 
@@ -55,7 +65,24 @@ class Solution:
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if (sum < target):
+                l += 1
+            elif (sum > target):
+                r -= 1
+            else:
+                return [l + 1, r + 1]
+        return []
+
+
+# Traceback (most recent call last):
+#   File "/box/main.py", line 44, in main
+#     raise TypeError(f"Your output was {output}, but the expected return type is List[int]")
+# TypeError: Your output was True, but the expected return type is List[int]
+
 #########################################################################################
 # 3Sum
 
@@ -90,6 +117,51 @@ class Solution:
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        for i, n in enumerate(nums):
+            l, r = 1 + i, len(nums) - 1
+
+            while nums[l] != n: 
+                l += 1
+            
+            while l < r:
+                sum = n + nums[l] + nums[r]
+                if 0 < sum:
+                    r -= 1
+                elif 0 > sum:
+                    l += 1
+                else:
+                    res.append[n, nums[l], nums[r]]
+                    
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
+        return res
         
 
 #########################################################################################
@@ -112,6 +184,20 @@ class Solution:
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
         
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        l, r = 0, len(heights) - 1
+        res = 0
+
+        while l < r:
+            area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r]:
+                l += 1
+            else:
+                r -= 1
+        return res
+
 
 #########################################################################################
 
