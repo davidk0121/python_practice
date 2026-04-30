6edd756bf0b04ee26f243aca49080934137b140f
Jiin Kim <>
2026-04-30 16:59:34 -0700

    4/30 update

--- topic/Sliding_Window.py
+++ topic/Sliding_Window.py
@@ -18,7 +18,18 @@
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        l, r = 0, 1
+        maxRes = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                sum = prices[r] - prices[l]
+                maxRes = max(sum, maxRes)
+                r += 1
+            else:
+                l = r 
+                r += 1
+        return maxRes
+
 class Solution:     
     def maxProfit(self, prices: List[int]) -> int:
         l, r = 0, 1
@@ -53,7 +64,7 @@ class Solution:
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
+        
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         charSet = set()
