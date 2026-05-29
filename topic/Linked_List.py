bef150a383fd42f6f3160df8d52cbe3060638034
Jiin Kim <>
2026-05-29 11:32:07 -0700

    5/29 update

--- topic/Linked_List.py
+++ topic/Linked_List.py
@@ -162,7 +162,8 @@ class Solution:
             second.next = temp1
             first = temp1
             second = temp2
-            
+        
+        
 #########################################################################################
 # Remove Node From End of Linked List
 # Medium
@@ -189,7 +190,45 @@ class Solution:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+        i = 1
+        while i <= n and right:
+            right = right.next
+            i += 1
         
+        while right:
+            left = left.next
+            right = right.next
+
+        #delete
+        left.next = left.next.next
+        return dummy.next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+
+        while right:
+            left = left.next
+            right = right.next
+
+        #delete
+        left.next = left.next.next
+        return dummy.next
+
+
 #########################################################################################
 
 # Copy Linked List with Random Pointer
