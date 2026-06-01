238ff92a18070c426cefeef195fe0e5ab9ab5fb3
davidk0121 <66999088+davidk0121@users.noreply.github.com>
2026-06-01 10:24:12 -0700

    5/31 update

--- topic/Linked_List.py
+++ topic/Linked_List.py
@@ -290,8 +290,22 @@ class Solution:
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
 
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if val1 else 0 
+            val2 = l2.val if val2 else 0 
 
+            sum = val1 + val2 + carry 
+            rem = sum % 10 
+            carry = sum // 10 
+            cur.next = ListNode(rem)
+
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            cur = cur.next
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
